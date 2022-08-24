351d1fb3f6d46b31e1a591d9677cfd2c1876cc0f
Thomas Peter <98786221+Sergei-007@users.noreply.github.com>
2022-08-24 12:25:43 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import HoroscopeContext from "./context/HoroscopeContext";
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+
+const Root = () => {
+  return (
+    <HoroscopeContext.Provider value={{ sign: "Leo" }}>
+      <App />
+    </HoroscopeContext.Provider>
+  );
+};
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Root />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
